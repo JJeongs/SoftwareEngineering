1f9eb32477d7eb2fe4a42b9bd4b9d9045ca3197c
JJeongs <sovely97@naver.com>
2018-11-20 16:34:29 +0900

    initial main menu

---
+++ ManagePersonalInfo/src/MainMenu.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MainMenu {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("************ Hello ***********");
+		while (true) {
+			System.out.println("\n< Main Menu >");
+			System.out.println("1. Manage contacts\n2. Manage To-Do list\n3. Manage appointments\n4. Manage notes\n5. Exit");
+			System.out.print("Select Menu: ");
+			int menu = sc.nextInt();
+			switch(menu) {
+			case 1: /*new ManageContacts()*/; break;
+			case 2: /*new ManageToDoList()*/; break;
+			case 3: /*new ManageAppointments()*/; break;
+			case 4: /*new ManageNotes()*/; break;
+			case 5: System.out.println("\n********** Good bye **********"); System.exit(0);
+			}
+		}
+	}
+
+}
